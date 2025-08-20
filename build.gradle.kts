8232acc37973b6334ec8a20ee95dc55efa7593c9
Alexander Grube <alexp.grube@googlemail.com>
2025-08-21 00:42:19 +0200

    update dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,9 @@ repositories {
 
 dependencies {
     implementation("io.undertow:undertow-core:2.3.18.Final")
-    implementation("com.alibaba.fastjson2:fastjson2:2.0.57")
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.58")
     implementation("org.postgresql:postgresql:42.7.7")
-    implementation("com.zaxxer:HikariCP:6.3.0")
+    implementation("com.zaxxer:HikariCP:7.0.2")
     implementation("io.github.cdimascio:dotenv-java:3.2.0")
     implementation("at.favre.lib:bcrypt:0.10.2")
     implementation("org.eclipse.collections:eclipse-collections-api:13.0.0")
@@ -26,9 +26,9 @@ dependencies {
     implementation("io.minio:minio:8.5.17")
     implementation("io.github.mojtabaj:c-webp:1.0.2")
     implementation("org.bytedeco:javacv-platform:1.5.12")
-    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.7")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.7")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.7")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
